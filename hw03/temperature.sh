6b907b196027b6cebfe8e059aaad36657faab2a0
David Mehl <mehlda@rose-hulman.edu>
2016-09-13 15:19:30 -0400

    another attempt

--- hw03/temperature.sh
+++ hw03/temperature.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-declare tempC 
-declare tempF
-tempC = i2cget -y 2 0x49 00
-tempF = $(($tempC * 9))
-tempF = $(($tempF / 5))
-tempF = $((tempF + 32))
+tempC = 'i2cget -y 2 0x49 00'
+$tempF = $(($tempC \* 9))
+$tempF = $(($tempF / 5))
+$tempF = $(($tempF + 32))
 echo $tempF
