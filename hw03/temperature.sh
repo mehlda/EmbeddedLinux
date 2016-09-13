084a572ed88173fb186cb7fc71a3d6980bd1dc2c
David Mehl <mehlda@rose-hulman.edu>
2016-09-13 15:14:36 -0400

    declared variables

--- hw03/temperature.sh
+++ hw03/temperature.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+declare tempC 
+declare tempF
 tempC = i2cget -y 2 0x49 00
-tempF = $(($tempC * 9 / 5 + 32))
+tempF = $(($tempC * 9))
+tempF = $(($tempF / 5))
+tempF = $((tempF + 32))
 echo $tempF
