3d386476e670aded25d9bf71fd838544d2545396
David Mehl <mehlda@rose-hulman.edu>
2016-09-14 15:46:15 -0400

    attempted to fix script

--- hw03/temperature.sh
+++ hw03/temperature.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-let temp = 'i2cget -y 2 0x49 00'
-let temp = temp \* 9
-let temp = temp / 5
-let temp = temp + 32
+let 'temp = i2cget -y 2 0x49 00'
+let 'temp = temp * 9'
+let 'temp = temp / 5'
+let 'temp = temp + 32'
 echo $temp
