3fa3627850d681293da5c302176b34115c4639f6
David Mehl <davidamehl@gmail.com>
2016-09-14 20:53:29 -0400

    Update install.sh

--- hw03/install.sh
+++ hw03/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-echo install readline-sync node.js package
-npm install readline-sync
+echo install i2c node.js package
+npm install i2c@0.2.1
 chmod +x etchAsketch.js
 echo Running...
+echo ready will display when the code is initialized
 ./etchAsketch.js
