e24f67e62696995748a6b973bb7cd7507e217f9d
David Mehl <davidamehl@gmail.com>
2016-09-18 18:01:45 -0400

    Update install.sh

--- hw03/install.sh
+++ hw03/install.sh
@@ -3,5 +3,5 @@ echo install i2c node.js package
 npm install i2c@0.2.1
 chmod +x etchAsketch.js
 echo Running...
-echo ready will display when the code is initialized
+echo Ready will display when the code is initialized and ready to use
 ./etchAsketch.js
