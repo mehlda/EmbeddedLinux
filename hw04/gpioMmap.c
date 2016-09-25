66345057b8f67c3e300e4ff26c0b5d09278d9111
David Mehl <mehlda@rose-hulman.edu>
2016-09-25 18:05:25 -0400

    Added commments and attempted to fix sig int handler

--- hw04/gpioMmap.c
+++ hw04/gpioMmap.c
@@ -1,6 +1,7 @@
 /*
  * gpioMmap.c 
  * Author: David Mehl
+ * Sig Int written by Mark A. Yoder
  * 
  * Simple program to manipulate GPIO using the mmap() command
 */
@@ -12,6 +13,7 @@
 #include <fcntl.h> 
 #include <signal.h>
 
+//Defines from the TRM
 #define GPIO0_BASE 0x44E07000
 #define GPIO1_BASE 0x4804C000
 #define GPIO2_BASE 0x481AC000
@@ -28,34 +30,49 @@
 #define SWITCH0 (1<<31) //P9_13, GPIO port 0
 #define SWITCH1 (1<<16) //P9_15, GPIO port 1
 
+//Variable to control loop
 int active = 1;
 
-void signal_handler(int sig){
+//Sig Int handler written by Mark A. Yoder
+void signal_handler(int sig)
+{
     printf( "\nCtrl-C pressed, cleaning up and exiting...\n" );
-	active = 0;
+	keepgoing = 0;
 }
 
+
 int main(){
+	// Use Dr. Yoder's Sig Int handler
+	signal(SIGINT, signal_handler);
+
+	//Declare the memory pointers
 	printf("Declaring Variables...\n");
 	volatile void * gpioAddr0, * gpioAddr1;
 	volatile unsigned int *gpioClearDataOutAddr0, * gpioClearDataOutAddr1;
 	volatile unsigned int * gpioSetDataOutAddr0,* gpioSetDataOutAddr1;
 	volatile unsigned int * gpioDataIn0, *gpioDataIn1;
+
+	//Get the memory map file
 	printf("Opening /dev/mem...\n");
 	int fd = open("/dev/mem", O_RDWR);
 
+	//Get the mapped pointers
 	printf("Calling mmap...\n");
 	gpioAddr0 = mmap(0, GPIO_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, GPIO0_BASE);
 	gpioAddr1 = mmap(0, GPIO_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, GPIO1_BASE);
 
+	//Build up the pointers we need
 	printf("Creating pointers...\n");
 	gpioSetDataOutAddr0 = gpioAddr0 + GPIO_SETDATAOUT;
 	gpioClearDataOutAddr0 = gpioAddr0 + GPIO_CLEARDATAOUT;
+
 	gpioSetDataOutAddr1 = gpioAddr1 + GPIO_SETDATAOUT;
 	gpioClearDataOutAddr1 = gpioAddr1 + GPIO_CLEARDATAOUT;
 
 	gpioDataIn0 = gpioAddr0 + GPIO_DATAIN;
 	gpioDataIn1 = gpioAddr1 + GPIO_DATAIN;
+
+	//Check the two switches/buttons, and set the usr leds when appropriate
 	printf("Starting...\n");
 	while(active){
 		if(*gpioDataIn0 & SWITCH0){
@@ -70,6 +87,7 @@ int main(){
 		}
 	}
 
+	//Unmap the memory and end the program
 	munmap((void *)gpioAddr0, GPIO_SIZE);
 	munmap((void *)gpioAddr1, GPIO_SIZE);
 	close(fd);
