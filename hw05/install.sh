53e9c07607c45939babf9fb399467fea644cf4f2
David Mehl <davidamehl@gmail.com>
2016-10-02 23:34:54 -0400

    Update install.sh

--- hw05/install.sh
+++ hw05/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo Installing needed packages...
 npm install require
-npm install i2c
+npm install i2c@0.2.1
 npm install util
 npm install request
 echo Finished installing packages
