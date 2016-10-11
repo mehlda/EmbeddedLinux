b9979cbc7ef5f2b3eb20eaf025832295d243755f
David Mehl <mehlda@rose-hulman.edu>
2016-10-11 17:34:16 -0400

    rewrote code to use adafruit library

--- hw06/stepperSearch.py
+++ hw06/stepperSearch.py
@@ -3,51 +3,73 @@
 # Simple control of stepper motor to find IR light sources
 
 #import py library
-from bbio import *
+import Adafruit_BBIO.GPIO as GPIO
+import Adafruit_BBIO.ADC as ADC
+
+ADC.setup()
 
 # define global variables/constants, mainly pin numbers
 # 'pt' will be short for phototransistor
-ptInputLeft = AIN0		# P9_39
-ptInputRight = AIN1		# P9_40
+ptInputLeft = "AIN0"		# P9_39
+ptInputRight = "AIN1"		# P9_40
 
-stepper0 = GPIO0_30		# P9_11
-stepper1 = GPIO0_31		# P9_13
-stepper2 = GPIO1_16		# P9_15
-stepper3 = GPIO1_19		# P9_16
+stepper0 = "GPIO0_30"		# P9_11
+stepper1 = "GPIO0_31"		# P9_13
+stepper2 = "GPIO1_16"		# P9_15
+stepper3 = "GPIO1_19"		# P9_16
 
-startButton = GPIO1_28	# P9_12
+startButton = "GPIO1_28"	# P9_12
 
 #use LEDS to mirror stepper output
-led0 = USR0
-led1 = USR1
-led2 = USR2
-led3 = USR3
+led0 = "USR0"
+led1 = "USR1"
+led2 = "USR2"
+led3 = "USR3"
 
 
+GPIO.add_event_detect("P9_12", GPIO.FALLING)
+
+if GPIO.event_detected("P9_12"):
+    print "event detected!"
 
 # create initialization function
 def setup():
 	# Start button input
-	pinMode(startButton, INPUT)
+	GPIO.setup(startButton, INPUT)
 
 	# Stepper outputs
-	pinMode(stepper0, OUTPUT)
-	pinMode(stepper1, OUTPUT)
-	pinMode(stepper2, OUTPUT)
-	pinMode(stepper3, OUTPUT)
+	GPIO.setup(stepper0, OUTPUT)
+	GPIO.setup(stepper1, OUTPUT)
+	GPIO.setup(stepper2, OUTPUT)
+	GPIO.setup(stepper3, OUTPUT)
 
 	# LED outputs
-	pinMode(led0, OUTPUT)
-	pinMode(led1, OUTPUT)
-	pinMode(led2, OUTPUT)
-	pinMode(led3, OUTPUT)
+	GPIO.setup(led0, OUTPUT)
+	GPIO.setup(led1, OUTPUT)
+	GPIO.setup(led2, OUTPUT)
+	GPIO.setup(led3, OUTPUT)
 
 def loop():
-	toggle(led0)
-	toggle(led1)
+	if GPIO.input(led0)
+		GPIO.output(led0, LOW)
+	else
+		GPIO.output(led0, HIGH)
+
+	if GPIO.input(led1)
+		GPIO.output(led1, LOW)
+	else
+		GPIO.output(led1, HIGH)
+
 	delay(500)
-	toggle(led2)
-	toggle(led3)
+	if GPIO.input(led2)
+		GPIO.output(led2, LOW)
+	else
+		GPIO.output(led2, HIGH)
+		
+	if GPIO.input(led3)
+		GPIO.output(led3, LOW)
+	else
+		GPIO.output(led3, HIGH)
 	delay(500)
 
 # Start
