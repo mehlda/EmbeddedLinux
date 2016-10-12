09ec49dbf40bf8cb73fb15d7d84fcb9610d8064f
David Mehl <mehlda@rose-hulman.edu>
2016-10-12 14:07:24 -0400

    went back to single step to test for pins

--- hw06/stepperSearch.py
+++ hw06/stepperSearch.py
@@ -15,10 +15,10 @@ ADC.setup()
 ptInputLeft = "P9_39"		# P9_39
 ptInputRight = "P9_40"		# P9_40
 
-stepper0 = "P9_11"		# P9_11
-stepper1 = "P9_13"		# P9_15
-stepper2 = "P9_15"		# P9_13
-stepper3 = "P9_16"		# P9_16
+stepper0 = "P9_15"		# P9_11
+stepper1 = "P9_16"		# P9_15
+stepper2 = "P9_11"		# P9_13
+stepper3 = "P9_13"		# P9_16
 
 startButton = "P9_12"	# P9_12
 
@@ -28,7 +28,7 @@ led1 = "USR1"
 led2 = "USR2"
 led3 = "USR3"
 
-counterClockwise = (0x3,0x6,0xC,0x9)
+counterClockwise = (0x1,0x2,0x4,0x8)
 
 
 
