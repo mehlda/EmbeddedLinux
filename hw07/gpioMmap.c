faf0e35a9b953f86a558d5f98c07dc978c96af33
David Mehl <mehlda@rose-hulman.edu>
2016-10-24 19:09:37 -0400

    modified gpioMmap.c to use the same pins as gpioNode.js

--- hw07/gpioMmap.c
+++ hw07/gpioMmap.c
@@ -26,11 +26,9 @@
 #define USR2 (1<<23) // GPIO port 1
 #define USR3 (1<<24) // GPIO port 1
 
-#define SWITCH0 (1<<31) //P9_13, GPIO port 0
-#define SWITCH1 (1<<16) //P9_15, GPIO port 1
+#define LEADER (1<<14) //P9_26, GPIO port 0
+#define FOLLOWER (1<<19) //P9_27, GPIO port 3
 
-//Variable to control loop
-int active = 1;
 
 int main(){
 
@@ -48,34 +46,28 @@ int main(){
 	//Get the mapped pointers
 	printf("Calling mmap...\n");
 	gpioAddr0 = mmap(0, GPIO_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, GPIO0_BASE);
-	gpioAddr1 = mmap(0, GPIO_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, GPIO1_BASE);
+	gpioAddr3 = mmap(0, GPIO_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, GPIO3_BASE);
 
 	//Build up the pointers we need
 	printf("Creating pointers...\n");
 	gpioSetDataOutAddr0 = gpioAddr0 + GPIO_SETDATAOUT;
 	gpioClearDataOutAddr0 = gpioAddr0 + GPIO_CLEARDATAOUT;
 
-	gpioSetDataOutAddr1 = gpioAddr1 + GPIO_SETDATAOUT;
-	gpioClearDataOutAddr1 = gpioAddr1 + GPIO_CLEARDATAOUT;
+	gpioSetDataOutAddr3 = gpioAddr3 + GPIO_SETDATAOUT;
+	gpioClearDataOutAddr3 = gpioAddr3 + GPIO_CLEARDATAOUT;
 
 	gpioDataIn0 = gpioAddr0 + GPIO_DATAIN;
-	gpioDataIn1 = gpioAddr1 + GPIO_DATAIN;
+	gpioDataIn3 = gpioAddr3 + GPIO_DATAIN;
 
 	//Check the two switches/buttons, and set the usr leds when appropriate
 	printf("Starting...\n");
-	while(active){
-		if(*gpioDataIn0 & SWITCH0){
-			*gpioSetDataOutAddr1 = USR2;
+	while(1){
+		if(*gpioDataIn0 & LEADER){
+			*gpioSetDataOutAddr3 = FOLLOWER;
 		} else {
-			*gpioClearDataOutAddr1 = USR2;
-		}
-		if(*gpioDataIn1 & SWITCH1){
-			*gpioSetDataOutAddr1 = USR3;
-		} else {
-			*gpioClearDataOutAddr1 = USR3;
+			*gpioClearDataOutAddr3 = FOLLOWER;
 		}
 	}
-
 	return 0;
 }
 
